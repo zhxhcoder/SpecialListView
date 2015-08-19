c9efffcd655400f745a62215a7a0901ec283fb9a
Michael <mrcoder@qq.com>
2015-08-19 19:09:09 +0800

    Reformat

--- src/com/michael/listview/feed/FeedMainActivity.java
+++ src/com/michael/listview/feed/FeedMainActivity.java
@@ -66,6 +66,7 @@ public class FeedMainActivity extends Activity {
         getMenuInflater().inflate(R.menu.feed_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
